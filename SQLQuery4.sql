23c18e5fda224c7472bf38e8210d06a2e6fdbeda
Shanker-2004 <kinjarapushanker04@gmail.com>
2025-08-10 21:57:55 +0530

    Update SQLQuery4.sql

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -1,31 +1,31 @@
-/*SELECT * FROM customers
+SELECT * FROM customers
 SELECT id,country FROM customers
 SELECT * FROM customers WHERE score>500
 SELECT * FROM customers WHERE score=500
 SELECT * FROM customers  ORDER BY score ASC
 SELECT * FROM customers ORDER BY score DESC
---SELECT * FROM customers ORDER BY country ASC,score DESC
---SELECT country,count(score) FROM customers GROUP BY country
---SELECT country,STDEV(score) FROM customers GROUP BY country
---SELECT country,AVG(score) FROM customers GROUP BY country
---SELECT country,sum(score) FROM customers GROUP BY country
---SELECT country,id ,varp(score) as total_score FROM customers GROUP BY country,id
---SELECT country,count(id) total_members,sum(score) as total_score FROM customers GROUP BY country
---SELECT country,sum(score) as total_score FROM customers where score>400 GROUP BY country HAVING sum(score)>800
---SELECT country,sum(score) as total_score FROM customers where score!=0 GROUP BY country HAVING sum(score)>400
---SELECT DISTINCT country FROM customers
+SELECT * FROM customers ORDER BY country ASC,score DESC
+SELECT country,count(score) FROM customers GROUP BY country
+SELECT country,STDEV(score) FROM customers GROUP BY country
+SELECT country,AVG(score) FROM customers GROUP BY country
+SELECT country,sum(score) FROM customers GROUP BY country
+SELECT country,id ,varp(score) as total_score FROM customers GROUP BY country,id
+SELECT country,count(id) total_members,sum(score) as total_score FROM customers GROUP BY country
+SELECT country,sum(score) as total_score FROM customers where score>400 GROUP BY country HAVING sum(score)>800
+SELECT country,sum(score) as total_score FROM customers where score!=0 GROUP BY country HAVING sum(score)>400
+SELECT DISTINCT country FROM customers
  SELECT TOP 3* FROM customers
---SELECT TOP 3* FROM customers order by score desc
---SELECT TOP 3* FROM customers order by score asc*/
+SELECT TOP 3* FROM customers order by score desc
+SELECT TOP 3* FROM customers order by score asc*/
 ------------------------------------------------------
 ------------------------------------------------------
 --all conditions performs in one query
---SELECT TOP 3  country ,sum(score) AS total_score FROM customers where score!=0 GROUP BY country HAVING sum(score)>350 ORDER BY total_score desc
+SELECT TOP 3  country ,sum(score) AS total_score FROM customers where score!=0 GROUP BY country HAVING sum(score)>350 ORDER BY total_score desc
 ------------------------------------------------------
 ------------------------------------------------------
- --SELECT 123 AS W
- --SELECT 11	 AS R
- --SELECT * ,'weebs' as r FROM customers
+ SELECT 123 AS W
+ SELECT 11	 AS R
+ SELECT * ,'weebs' as r FROM customers
  -----------------------------------------------------
  -----------------------------------------------------
  --ddl queries
@@ -123,6 +123,7 @@ where P_NAME LIKE'_A'
 SELECT * FROM raju
 where P_NAME LIKE'%I%'
 --
+
 
 
 
